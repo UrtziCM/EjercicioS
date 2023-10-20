a545c3c4e9c3614efd1965f3612c10478382ec4b
Urtzi <urcrmedev@gmail.com>
2023-10-20 13:48:32 +0200

    UPDATE module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,12 @@
  * 
  */
 module EjercicioS {
+	requires java.sql;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	opens tablaAnimal to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	opens model to javafx.base,javafx.controls;
+	opens controllers to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
 }
